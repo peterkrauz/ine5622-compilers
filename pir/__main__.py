6ae881d116aa72b18d27d0329d6b78c13c53d73d
Gabriel Donadel Dall'Agnol <gabriel@jungledevs.com>
2021-04-14 20:55:30 -0300

    feat: Update python files to support pir language

--- pir/__main__.py
+++ pir/__main__.py
@@ -4,15 +4,15 @@ import argparse
 from ast_decompiler import decompile
 
 try:
-    from .parser.cmmLexer import cmmLexer
-    from .parser.cmmParser import cmmParser
+    from .parser.pirLexer import pirLexer
+    from .parser.pirParser import pirParser
 except ImportError:
     # Generate ANTLR parser from g4 file
     from .generate_parser import generate_parser
-    generate_parser(path.join(path.dirname(__file__), 'parser/cmm.g4'))
+    generate_parser(path.join(path.dirname(__file__), 'parser/pir.g4'))
 
-    from .parser.cmmLexer import cmmLexer
-    from .parser.cmmParser import cmmParser
+    from .parser.pirLexer import pirLexer
+    from .parser.pirParser import pirParser
 
 # import astVisitor after the try-except
 from .astVisitor import astVisitor
@@ -21,29 +21,29 @@ from .astVisitor import astVisitor
 def main():
 
     # Argument parser
-    parser_args = argparse.ArgumentParser(prog='cmm', description='C-- interpreter')
+    parser_args = argparse.ArgumentParser(prog='pir', description='C-- interpreter')
     parser_args.add_argument('input', type=str, help='source code')
     parser_args.add_argument('-o', metavar='', type=str, default=None, help='python output')
 
-    args = parser_args.parse_args() 
+    args = parser_args.parse_args()
     #################################
 
     input_stream = FileStream(args.input)
 
-    lexer = cmmLexer(input_stream)
+    lexer = pirLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = cmmParser(stream)
+    parser = pirParser(stream)
 
     tree = parser.start() # Get AST
 
-    # Transverse AST to generate python ast 
+    # Transverse AST to generate python ast
     visitor = astVisitor(args.input)
     ast = visitor.visitStart(tree)
 
-    code = compile(source=ast, filename=args.input, mode='exec') 
+    code = compile(source=ast, filename=args.input, mode='exec')
 
     exec(code, globals())
-    
+
     if args.o:
         with open(args.o, 'w') as file:
             file.write(decompile(ast))
